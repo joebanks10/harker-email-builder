f46c2711f6d8e5e10f5ec086a5b9a234c2455945
Joe Banks <joeb@harker.org>
2016-03-16 10:22:09 -0700

    Rearrange dependencies

--- load.php
+++ load.php
@@ -16,6 +16,6 @@ define('ROOT_IMG_DIR_URL', ROOT_URL . 'assets/img/');
 require_once ROOT_DIR . "/vendor/autoload.php";
 
 // include author files
-require_once ROOT_DIR . "/inc/class-email.php";
 require_once ROOT_DIR . "/inc/class-rss-feed.php";
 require_once ROOT_DIR . "/inc/class-ics-feed.php";
+require_once ROOT_DIR . "/inc/class-email.php";
