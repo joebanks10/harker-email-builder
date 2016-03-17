6aa8eb94cd1f7011f57cbc9b1577c0174f2045a3
Joe Banks <joeb@harker.org>
2016-03-17 12:48:58 -0700

    Update letter type template.

--- templates/types/letter/index.php
+++ templates/types/letter/index.php
@@ -9,7 +9,7 @@ $dir_name = basename(__DIR__);
 $config = array(
     'email_dir' => dirname(__FILE__),
     'stylesheet_url' => ROOT_URL . 'assets/css/style.css', // URL to email's stylesheet
-    'stylesheet_addons_url' => ROOT_URL . "emails/$dir_name/assets/css/addons.css", // URL to email's addons stylesheet
+    'stylesheet_addons_url' => ROOT_URL . "templates/types/$dir_name/assets/css/addons.css", // URL to email's addons stylesheet
     'img_dir_url' => ROOT_URL . 'assets/img/' // URL to email's images folder
 );
 
