c93073da1a1cfddae44eb5cb8b22d2197faafc69
Joe Banks <joeb@harker.org>
2016-03-17 08:55:07 -0700

    Add header-bar and footer modules to starter. Simplify starter index file.

--- emails/_starter/index.php
+++ emails/_starter/index.php
@@ -3,15 +3,13 @@
 // include the magic sauce
 include "../../load.php";
 
-$path = dirname(__FILE__);
 $dir_name = basename(__DIR__);
 
+// configure URL paths here
 $config = array(
-    'email_dir' => $path,
-
-    // configure URL paths here
+    'email_dir' => dirname(__FILE__),
     'stylesheet_url' => ROOT_URL . 'assets/css/style.css', // URL to email's stylesheet
-    'stylesheet_addons_url' => ROOT_URL . "emails/$dir_name/assets/css/addons.css", // URL to addons stylesheet
+    'stylesheet_addons_url' => ROOT_URL . "emails/$dir_name/assets/css/addons.css", // URL to email's addons stylesheet
     'img_dir_url' => ROOT_URL . 'assets/img/' // URL to email's images folder
 );
 
