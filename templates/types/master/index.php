da555147e0639265b51b88e641385a4fe20b64c9
Joe Banks <joeb@harker.org>
2016-04-08 13:34:18 -0700

    Add list and header elements.

--- templates/types/master/index.php
+++ templates/types/master/index.php
@@ -10,8 +10,8 @@ $email_url = ROOT_URL . 'emails/' . basename(__DIR__);
 $config = array(
     'email_dir' => $email_path,
     'url' => $email_url,
-    'stylesheet_addons_url' => $email_url . '/assets/css/addons.css', // URL to email's addons stylesheet
     'stylesheet_url' => ROOT_URL . 'assets/css/style.css', // URL to email's stylesheet
+    'stylesheet_addons_url' => $email_url . '/assets/css/addons.css', // URL to email's addons stylesheet
     'img_dir_url' => ROOT_URL . 'assets/img/' // URL to email's images folder
 );
 
