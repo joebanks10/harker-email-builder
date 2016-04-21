a65df6ca9b24ebf299ad409dac70fefab79cca0d
Joe Banks <joeb@harker.org>
2016-04-21 13:08:39 -0700

    Fix reference to url_parts

--- inc/class-email.php
+++ inc/class-email.php
@@ -160,7 +160,7 @@ class Email {
 
     static public function get_email_url() {
         $url_parts = explode('?', $_SERVER['REQUEST_URI']);
-        $url = $uri_parts[0]; // URL w/o arguments
+        $url = $url_parts[0]; // URL w/o arguments
 
         return "http://$_SERVER[HTTP_HOST]$url";
     }
