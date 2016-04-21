48af42f54989dedf32e600942f48f499aaa9d357
Joe Banks <joeb@harker.org>
2016-04-21 11:28:40 -0700

    Turn debugging on for staging

--- load.php
+++ load.php
@@ -1,10 +1,8 @@
 <?php
 
 // debug
-if ( $_SERVER['SERVER_NAME'] == 'localhost' ) {
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-}
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 // constants
 define('ROOT_DIR', dirname(__file__));
