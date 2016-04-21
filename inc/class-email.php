deef121b5f38917d38c427e3a0286084aec461df
Joe Banks <joeb@harker.org>
2016-04-21 13:04:32 -0700

    Remove arguments from url when getting email url

--- inc/class-email.php
+++ inc/class-email.php
@@ -159,7 +159,10 @@ class Email {
     }
 
     static public function get_email_url() {
-        return "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $url_parts = explode('?', $_SERVER[REQUEST_URI]);
+        $url = $uri_parts[0]; // URL w/o arguments
+
+        return "http://$_SERVER[HTTP_HOST]$url";
     }
 
     // TEMPLATE FUNCTIONS
